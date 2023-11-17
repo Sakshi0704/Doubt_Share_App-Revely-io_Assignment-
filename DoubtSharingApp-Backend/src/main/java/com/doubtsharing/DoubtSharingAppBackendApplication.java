150dbff508d6a61fcd248e2cf129da91f438df06
Km Sakshi <112800906+Sakshi0704@users.noreply.github.com>
2023-11-17 12:52:30 +0530

    Update DoubtSharingAppBackendApplication.java

--- DoubtSharingApp-Backend/src/main/java/com/doubtsharing/DoubtSharingAppBackendApplication.java
+++ DoubtSharingApp-Backend/src/main/java/com/doubtsharing/DoubtSharingAppBackendApplication.java
@@ -2,8 +2,10 @@ package com.doubtsharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DoubtSharingAppBackendApplication {
 
 	public static void main(String[] args) {
